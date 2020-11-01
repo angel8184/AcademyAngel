98fe4bee4fa143f590a49219154b2a01c26283b6
angel <angel.huang@thinkpower-info.com>
2020-11-01 11:20:58 +0800

    補上WAR檔打包。

--- src/main/java/com/academy/AngelApplication.java
+++ src/main/java/com/academy/AngelApplication.java
@@ -1,13 +1,20 @@
 package com.academy;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"com.academy.dao", "com.academy.service", "com.academy.vo"})
-public class AngelApplication {
+public class AngelApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+		return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AngelApplication.class, args);
